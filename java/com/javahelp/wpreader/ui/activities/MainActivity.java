4724b07682ab4b82b1d57f238165180e3b5e7a2c
antonoctopod <a.grechushnikov@octopod.com>
2016-07-14 16:46:23 +0300

    create project

---
+++ java/com/javahelp/wpreader/ui/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.javahelp.wpreader.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.javahelp.wpreader.R;
+
+public class MainActivity
+        extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
